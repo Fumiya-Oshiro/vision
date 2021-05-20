c9a0c8a3f02157fe629c9ac6a92b3c5e9008203f
Fumiya.Oshiro <shidonisanhatigo@gmail.com>
2021-05-20 13:51:27 +0900

    app.py編集 importらへんとシメの行

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask, render_template
+import os
+
+import sqlite3
+
+from flask import Flask, render_template, request, redirect, session
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # Flask が持っている開発用サーバーを、実行します。
+    app.run(debug=True)
