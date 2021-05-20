f419426283aa87c76fb8a09c97b008afccfec9c8
Fumiya.Oshiro <shidonisanhatigo@gmail.com>
2021-05-20 16:38:04 +0900

    フォルダアップ機能のテスト

--- app.py
+++ app.py
@@ -4,13 +4,48 @@ import sqlite3
 
 from flask import Flask, render_template, request, redirect, session
 
+from werkzeug.utils import secure_filename
+
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("toppage.html")
+
+
+# ファイル容量上限 : 1MB
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+
+@app.route('/', methods=['GET'])
+def get():
+	return render_template('index.html', \
+		title = 'Form Sample(get)', \
+		message = '画像を選択して下さい。', \
+		flag = False)
+
+@app.route('/', methods=['POST'])
+def post():
+	
+	# ファイルのリクエストパラメータを取得
+	f = request.files.get('image')
+	
+	# ファイル名を取得
+	filename = secure_filename(f.filename)
+	
+	# ファイルを保存するディレクトリを指定
+	filepath = 'static/img/' + filename
+	
+	# ファイルを保存する
+	f.save(filepath)
+	
+	return render_template('index.html', \
+		title = 'Form Sample(post)', \
+		message = 'アップロードされた画像({})'.format(filename), \
+		flag = True, \
+		image_name = filename, \
+		image_url = filepath)
 
 if __name__ == "__main__":
     # Flask が持っている開発用サーバーを、実行します。
